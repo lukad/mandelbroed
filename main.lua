8016f3e94a367192b3754f9d4de8199a0c30220b
Luka Dornhecker <luka.dornhecker@gmail.com>
2019-04-23 08:19:17 +0200

    wip

--- main.lua
+++ main.lua
@@ -1,11 +1,17 @@
 local width, height = love.graphics.getDimensions()
 local shader
 local palettes = {}
-local palette_names = {"palette.png"}
+local palette_names = {"palette.png", "palette02.png", "palette03.png"}
 local scale = 1
-local center = {0.79574045120594, 0.184822245514133}
+local center = { 0.79574045120594, 0.184822245514133}
 local iter = 400
-local palette_index = 1
+local palette_index = 3
+local dragging = {
+  active = false,
+  x = 0,
+  y = 0
+}
+local scrollv = 0
 
 function love.load()
   shader = love.graphics.newShader("shader.glsl")
@@ -53,4 +59,39 @@ function love.update(dt)
   elseif love.keyboard.isDown("lshift") then
     scale = scale + dt * scale
   end
+
+  if dragging.active then
+    local mx, my = love.mouse.getPosition()
+    local dx = dragging.x - mx
+    local dy = dragging.y - my
+    center[1] = center[1] - dx * scale * 0.001
+    center[2] = center[2] - dy * scale * 0.001
+    dragging.x = mx
+    dragging.y = my
+  end
+
+  scale = scale - scrollv * dt * scale
+  scrollv = scrollv - scrollv * math.min(dt * 10, 1)
+
+  if (scrollv > 0 and scrollv < 0.1) or (scrollv < 0 and scrollv > -0.1) then
+    scrollv = 0
+  end
+end
+
+function love.mousepressed(x, y, button)
+  if button == 1 then
+    dragging.x = x
+    dragging.y = y
+    dragging.active = true
+  end
+end
+
+function love.mousereleased(x, y, button)
+  if button == 1 then
+    dragging.active = false
+  end
+end
+
+function love.wheelmoved(dx, dy)
+  scrollv = scrollv + dy
 end
